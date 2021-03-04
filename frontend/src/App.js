9d1e09a00ab05baf05c3e9c12cf0ec490d94080a
gGaboo99 <gerardo_gabriell@hotmail.com>
2021-03-04 00:20:35 -0600

    Agregue barra de navegacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/imagenes/logo.svg';
+import './assets/css/App.css';
+import Navbar from './componentes/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
